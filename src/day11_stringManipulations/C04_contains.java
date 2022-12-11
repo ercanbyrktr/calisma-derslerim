17d37113299c48995e4695c163969cad07198d0f
Ercan Bayraktar <ercan.byrktr.545435@gmail.com>
2022-12-11 15:57:08 +0300

    11/12/2022

--- src/day11_stringManipulations/C04_contains.java
+++ src/day11_stringManipulations/C04_contains.java
@@ -1,2 +1,31 @@
-package day11_stringManipulations;public class C04_contains {
+package day11_stringManipulations;
+
+public class C04_contains {
+    public static void main(String[] args) {
+
+        /*
+        Soru2) Kullanicidan bir cumle isteyin.
+        Cumle "buyuk" kelimesi iceriyorsa tum cumleyi buyuk harf olarak,
+        "kucuk" kelimesi iceriyorsa tum cumleyi kucuk harf olarak yazdirin,
+        iki kelimeyi de icermiyorsa "Cumle kucuk ya da buyuk kelimesi icermiyor" yazdirin.
+         */
+
+        String cumle="Java buyuk, dunya kucuk";
+        cumle=cumle.toLowerCase();
+        // requirements'de buyuk harf kucuk harf hassasiyeti hakkında birsey soylenmemiş
+        // ikinci olarak da her iki kelimeyi de icerme durumu acıklanmamış
+        // bu durumda görevi bize kim verdiyse ona sormak lazim
+
+        // ek requirements : ikisini de iceriyorsa "Karar ver buyuk mu yazdirayim, kucuk mu?"
+        // case sensitive olmasin
+        if (cumle.contains("kucuk")&&cumle.contains("buyuk")){
+            System.out.println("Karar ver buyuk mu yazdirayim, kucuk mu?");
+        }else if (cumle.contains("kucuk")){
+            System.out.println(cumle.toLowerCase());
+        }else if(cumle.contains("buyuk")){
+            System.out.println(cumle.toUpperCase());
+        }else{
+            System.out.println("Cumle kucuk yada buyuk kelimesi icermiyor");
+        }
+    }
 }
--- src/day11_stringManipulations/C06_endsWith.java
+++ src/day11_stringManipulations/C06_endsWith.java
@@ -1,2 +1,23 @@
-package day11_stringManipulations;public class C06_endsWith {
+package day11_stringManipulations;
+
+public class C06_endsWith {
+    public static void main(String[] args) {
+         /*
+        Soru 1) Kullanicidan email adresini girmesini isteyin,
+        mail @gmail.com icermiyorsa "Lutfen gmail adresi giriniz",
+        @gmail.com ile bitiyorsa "Email adresiniz kaydedildi",
+        @gmail.com ile bitmiyorsa lutfen yazimi kontrol edin yazdirin
+
+         */
+
+        String input="ercan@gmail.com";
+
+        if (!input.contains("@gmail.com")){
+            System.out.println("Lutfen gmail adresi giriniz");
+        }else if(input.endsWith("@gmail.com")){
+            System.out.println("Email adresiniz kaydedildi");
+        }else{
+            System.out.println("Lutfen yazimi kontrol edin");
+        }
+    }
 }
