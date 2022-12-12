151b6b45b5732912aeb4a1fe62e9eeef48539a5f
Ercan Bayraktar <ercan.byrktr.545435@gmail.com>
2022-12-12 19:37:50 +0300

    12/12/2022

---
+++ src/day12_stringManipulations/C03_substring.java
@@ -0,0 +1,33 @@
+package day12_stringManipulations;
+
+public class C03_substring {
+    public static void main(String[] args) {
+
+        String isim="Suleyman";
+        String soyisim="Karanfil";
+        String kartNo="1234 6589 7458 9658";
+
+        System.out.println(isim.substring(3)); //eyman
+        System.out.println(soyisim.substring(soyisim.length()-3));//fil
+        System.out.println(isim.substring(2,4));//le
+
+        //isim ve soyismin ilk harfi buyuk harf, geriye kalan *
+        // kredi kartinin ilk 4 rakami gorunsun geriye kalan *
+
+        String isimIlkharf=isim.substring(0,1).toUpperCase();
+        String isimGeriyeKalanlar=isim.substring(1).replaceAll("\\w","*");
+
+        String soyisimIlkharf=soyisim.substring(0,1).toUpperCase();
+        String soyisimGeriyekalanlar=soyisim.substring(1).replaceAll("\\w","*");
+
+        String kkIlk4=kartNo.substring(0,4);
+        String kkGeriyeKalanlar=" **** **** ****";
+
+        System.out.println(isimIlkharf+isimGeriyeKalanlar+" "+
+                soyisimIlkharf+soyisimGeriyekalanlar+" "+
+                kkIlk4+kkGeriyeKalanlar);
+
+
+
+    }
+}
