c41faf3409e62fc2fd813e785cf08afa002dab0d
Ercan Bayraktar <ercan.byrktr.545435@gmail.com>
2022-12-19 11:12:49 +0300

    19/12/2022

---
+++ src/day18_forLoop/C01_ForLoop.java
@@ -0,0 +1,19 @@
+package day18_forLoop;
+
+public class C01_ForLoop {
+    public static void main(String[] args) {
+        // verilen bir pozitif tam sayinin
+        // pozitif tam bolenlerini yazdiralim
+
+        int input=20;
+
+        for (int i = 1; i <=input ; i++) {
+
+            if (input%i==0){
+                System.out.print(i+ " ");
+            }
+
+        }
+
+    }
+}
