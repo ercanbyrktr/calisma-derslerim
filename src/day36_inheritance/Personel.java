ad7957f192c9a2e672435c13d2354a9db78cf3b0
Ercan Bayraktar <ercan.byrktr.545435@gmail.com>
2023-01-08 18:13:18 +0300

    08/01/2023

---
+++ src/day36_inheritance/Personel.java
@@ -0,0 +1,28 @@
+package day36_inheritance;
+
+public class Personel {
+    /*
+    Eger parent class olacak sekilde tasarladiginiz bir class varsa
+    veya ilerde bu class'i parent yapmak isteyenler olabilir diyorsaniz
+    variable ve method'larin
+    access modifier'ini proteced
+     */
+    protected int persNo;
+    protected String isim="Isim belirtilmedi";
+    protected String departman="Departman belirtilmedi";
+
+
+    protected void maas(){
+        System.out.println("Tum personelin maasi vardir");
+    }
+
+    protected void mesai(){
+        System.out.println("Tum personel statusune gore mesai yapar");
+    }
+
+    protected void fazlaMesaiUcreti(){
+        System.out.println("personel fazla mesai ucretini statusune gore alir");
+    }
+
+
+}
